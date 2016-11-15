acaddb5fef588d3e2bf00340e501f5c23e4cf2f2
Maxime Biloé <maxime.biloe@prestashop.com>
2016-11-15 11:30:09 +0100

    // Version updated to v1.0.1

--- ps_advertising.php
+++ ps_advertising.php
@@ -48,7 +48,7 @@ class Ps_Advertising extends Module implements WidgetInterface
     {
         $this->name = 'ps_advertising';
         $this->tab = 'advertising_marketing';
-        $this->version = '1.0.0';
+        $this->version = '1.0.1';
         $this->author = 'PrestaShop';
         $this->need_instance = 0;
 
